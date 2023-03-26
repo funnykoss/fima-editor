b1a1e8fe54097e25678c4a074f005c1be807edd8
funnykoss <75586141+funnykoss@users.noreply.github.com>
2023-03-26 15:26:53 +0200

    strukture

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,18 +1,21 @@
 import { Outlet } from "react-router-dom";
+import Container from "../Container";
 import Header from "../Header/Header";
 import s from "./Layout.module.css";
 const Layout = () => {
   return (
     <>
-      <div className={s.app}>
-        <header className={s.header}>
-          <Header />
-        </header>
-        <main>
-          <Outlet />
-        </main>
-      </div>
-      <footer></footer>
+      <Container>
+        <div className={s.app}>
+          <header className={s.header}>
+            <Header />
+          </header>
+          <main>
+            <Outlet />
+          </main>
+          <footer></footer>
+        </div>
+      </Container>
     </>
   );
 };
